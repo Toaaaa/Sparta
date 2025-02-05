e67a4a90cce7e7fcdbb7ed780fb22cc50b4368ae
kim minjun <k980128@naver.com>
2025-02-05 19:09:18 +0900

    syntex

---
+++ Code/Code/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Code
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            String s;
+            s = Console.ReadLine();
+            int hp = int.Parse(s);
+            //Console.Clear();
+            if(hp >=0 && hp <= 1000)
+                Console.WriteLine(solution(hp));
+        }
+
+        static int solution(int hp)
+        {
+            int answer = 0;
+
+            answer += hp / 5; 
+            hp %= 5;          
+            answer += hp / 3; 
+            hp %= 3;          
+            answer += hp;
+
+            return answer;
+        }
+
+    }
+}
