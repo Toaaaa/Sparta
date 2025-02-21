988fc3c58e8c0a58f1448c2036677ffad5faabc9
kim minjun <k980128@naver.com>
2025-02-21 13:38:00 +0900

    .

---
+++ Code/Code/code/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Formats.Asn1;
+
+
+namespace code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(3,4, [1, 2, 3, 1, 2, 3, 1]));
+        }
+        static public int solution(int k, int m, int[] score)
+        {
+            int answer = 0;
+            int[] arr = new int[score.Count() - score.Count() % m];
+            score = score.OrderByDescending(x => x).ToArray();
+            Array.Copy(score, arr, arr.Length);
+            arr = arr.OrderBy(x => x).ToArray();
+            for (int i = 0; i < arr.Length; i += m)
+            {
+                answer += arr[i] * m;
+            }
+            return answer;
+        }
+
+    }
+
+}
--- Code/Code/code/obj/Debug/net8.0/code.AssemblyInfo.cs
+++ Code/Code/code/obj/Debug/net8.0/code.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad9e3db557d03f6ed817fde935a8b4b2c34f2610")]
-[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
+[assembly: System.Reflection.AssemblyProductAttribute("code")]
+[assembly: System.Reflection.AssemblyTitleAttribute("code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
