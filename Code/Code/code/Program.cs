ff378bff294de2fe63a267364efad3fde325bd04
kim minjun <k980128@naver.com>
2025-07-04 16:02:27 +0900

    Coding Test
    https://school.programmers.co.kr/learn/courses/30/lessons/12927

--- Code/Code/code/Program.cs
+++ Code/Code/code/Program.cs
@@ -6,77 +6,40 @@ namespace code
 {
     internal class Program
     {
+        /// <summary>
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/12927
+        /// </summary>
+
         public class Solution
         {
-            public int solution(string[] board)
+            public long solution(int n, int[] works)
             {
-                // 길이가 3인 board를 2차원 배열로 변환.
-                int rows = board.Length;
-                int cols = board[0].Length;
-                char[,] result = new char[rows, cols];
+                if(n >= works.Sum()) return 0; // 모든 작업을 완료할 수 있는 경우, 0 반환
 
-                for (int i = 0; i < rows; i++)
+                while (n > 0)
                 {
-                    for (int j = 0; j < cols; j++)
-                    {
-                        result[i, j] = board[i][j];
-                    }
+                    int max = works.Max();
+                    if (max == 0) break; // 모든 작업이 완료된 경우
+
+                    works = DecreaseMax(max, works);
+                    n--;
                 }
 
-                // 조건 처리 시작
-
-                // 1. 모든 칸이 비어있음 == 시작하지 않음 => 가능한 조건.
-                if (board.All(x => x == "...")) return 1;
-
-                // 2. X의 개수가 O의 개수보다 많거나, O의 개수가 X의 개수보다 2개이상(많은 경우) 차이나는 경우
-                int xCount = 0;
-                int oCount = 0;
-                for (int i = 0; i < rows; i++)
+                long answer = 0;
+                for(int i = 0; i < works.Length; i++)
                 {
-                    for (int j = 0; j < cols; j++)
-                    {
-                        if (result[i, j] == 'X') xCount++;
-                        else if (result[i, j] == 'O') oCount++;
-                    }
+                    answer += (long)Math.Pow(works[i], 2); // 오버플로우 방지 long 캐스팅
                 }
-                if (!(oCount == xCount || oCount == xCount + 1)) return 0;
-
-                // 각각의 승리 여부 체크
-                bool xWin = IsWin(result, 'X');
-                bool oWin = IsWin(result, 'O');
-
-
-                // 3. X가 이긴 경우 O의 개수가 X의 개수와 같지 않음 => 불가능한 경우
-                if (xWin && xCount != oCount) return 0;
-                // 4. O가 이긴 경우 X의 개수보다 1개 많음이 아닌 경우 => 불가능한 경우
-                if (oWin && oCount != xCount + 1) return 0;
-                // 5. X와 O가 동시에 이긴 경우 => 불가능한 경우
-                if(xWin&&oWin) return 0;
-
-
-                // 나머지
-                return 1;
+                return answer;
             }
 
-            public bool IsWin(char[,] board, char player)
+            public int[] DecreaseMax(int max,int[] work)
             {
-                for (int i = 0; i < 3; i++)
-                {
-                    // 가로
-                    if (board[i, 0] == player && board[i, 1] == player && board[i, 2] == player)
-                        return true;
-                    // 세로
-                    if (board[0, i] == player && board[1, i] == player && board[2, i] == player)
-                        return true;
-                }
+                int index = Array.IndexOf(work, max);
 
-                // 대각선
-                if (board[0, 0] == player && board[1, 1] == player && board[2, 2] == player)
-                    return true;
-                if (board[0, 2] == player && board[1, 1] == player && board[2, 0] == player)
-                    return true;
+                work[index] = max - 1;
 
-                return false;
+                return work;
             }
         }
     }
