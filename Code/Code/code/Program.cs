f34539f2056c55a40c7ba7fcf927c8538eba07d7
kim minjun <k980128@naver.com>
2025-07-08 23:19:40 +0900

    Code Test
    https://school.programmers.co.kr/learn/courses/30/lessons/87390

--- Code/Code/code/Program.cs
+++ Code/Code/code/Program.cs
@@ -7,35 +7,30 @@ namespace code
     internal class Program
     {
         /// <summary>
-        /// https://school.programmers.co.kr/learn/courses/30/lessons/12921
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/87390
         /// </summary>
 
         public class Solution
         {
-            public int solution(int n)
+            public int[] solution(int n, long left, long right)
             {
-                if (n < 2) return 0;
+                int length = (int)(right - left + 1); // right - left 는 10^5 보다 작아서 int 변환 가능.
+                int[] answer = new int[length];
 
-                bool[] isPrime = new bool[n + 1];
-                for (int i = 2; i <= n; i++) isPrime[i] = true;
-
-                for (int i = 2; i * i <= n; i++)
-                {
-                    if (!isPrime[i]) continue;
-
-                    for (int j = i * i; j <= n; j += i)
-                    {
-                        isPrime[j] = false;
-                    }
-                }
-
-                int answer = 0;
-                for (int i = 2; i <= n; i++)
-                {
-                    if (isPrime[i]) answer++;
+                for (int i = 0; i < length; i++)
+                {              
+                    answer[i] = GetValue(n, left + i);
                 }
                 return answer;
             }
+            public int GetValue(int n, long index)
+            {
+                long row = index / n;
+                long col = index % n;
+
+                return (int)Math.Max(row, col) + 1;
+            }
+
         }
     }
 }
