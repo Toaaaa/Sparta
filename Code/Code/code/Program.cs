9a37ee65a406a6a2d3216956fb80185b57da54a8
kim minjun <k980128@naver.com>
2025-07-31 21:47:08 +0900

    Coding test
    https://school.programmers.co.kr/learn/courses/30/lessons/178870

--- Code/Code/code/Program.cs
+++ Code/Code/code/Program.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Formats.Asn1;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 
 
@@ -8,86 +11,247 @@ namespace code
     internal class Program
     {
         /// <summary>
-        /// https://school.programmers.co.kr/learn/courses/30/lessons/148653
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/178870
         /// </summary>
 
         public class Solution
         {
-            public int solution(int storey)
+            public int[] solution(int[] sequence, int k)
             {
-                int answer = 0;
-                while (storey / 10 > 0)
+                int[] answer = new int[] { };
+                for(int i = 0; i < sequence.Length; i++)
                 {
-                    Stone(ref storey ,storey % 10, ref answer);
-                    storey /= 10;
+                    for(int j = i; j < sequence.Length; j++)
+                    {
+                        if(Sum(sequence, i, j) == k)
+                        {
+                            if(answer.Length == 0 || (j - i < answer[1] - answer[0]))
+                            {
+                                answer = new int[] { i, j };
+                            }
+                        }
+                    }
                 }
-                LastStone(storey, ref answer);
                 return answer;
             }
 
-            void Stone(ref int ori,int num, ref int ans)
+            public int Sum(int[] sequence, int start, int end)
             {
-                if(num == 0)
+                int sum = 0;
+                for(int i = start; i <= end; i++)
+                    sum += sequence[i];
+
+                return sum;
+            }
+        }
+
+        public class Solution2
+        {
+            public int[] solution(int[] sequence, int k)
+            {
+                int[] answer = new int[] { };
+                // 이분 탐색으로 한번 줄이고 줄인 배열 시작 범위를 i 에 넣기.
+                int left = 0;
+                int right = sequence.Length - 1;
+                int start = right;
+
+                while (left <= right)
                 {
-                    return;
-                }
-                if(num <= 4)
-                {
-                    ans += num;
-                    return;
-                }
-                if(num == 5)
-                {
-                    if (ori / 10 != 0)
+                    int mid = (left + right) / 2;
+                    long sums = Sum(sequence, left, mid);
+
+                    if (sums <= k || sums == -1)
                     {
-                        if((ori /10) % 10 >= 5)
-                        {
-                            ans += 5;
-                            ori += 10;
-                        }
-                        else
-                        {
-                            ans += 5;
-                        }
+                        start = mid;
+                        right = mid - 1;
                     }
                     else
                     {
-                        ans += 5;
+                        left = mid + 1;
                     }
-                    return;
                 }
-                if(num >= 6)
+
+                for (int i = start; i < sequence.Length; i++)
                 {
-                    ans += 10 - num;
-                    ori += 10;
-                    return;
+                    for (int j = i; j < sequence.Length; j++)
+                    {
+                        if (Sum(sequence, i, j) == k)
+                        {
+                            if (answer.Length == 0 || (j - i < answer[1] - answer[0]))
+                            {
+                                answer = new int[] { i, j };
+                            }
+                        }
+                    }
                 }
+                return answer;
             }
 
-            void LastStone(int num, ref int ans)
+            public long Sum(int[] sequence, int start, int end)
             {
-                if (num == 0)
+                long sum = 0;
+                for (int i = start; i <= end; i++)
                 {
-                    return;
+                    sum += sequence[i];
+                    if (sum > int.MaxValue)
+                        return -1; 
+
                 }
-                if (num <= 5)
+
+                return sum;
+            }
+        }
+
+        public class Solution3
+        {
+            public int[] solution(int[] sequence, int k)
+            {
+                int[] answer = new int[] { };
+                HashSet<Tuple<int,int>> pairs = new HashSet<Tuple<int, int>>();
+                int minDiff = int.MaxValue;
+                int tupleStart = 0;
+                int left = 0;
+                int right = 0;
+                long sum = sequence[left];
+
+
+                while (left < sequence.Length && right < sequence.Length)
                 {
-                    ans += num;
-                    return;
-                }  
-                if (num >= 6)
-                {
-                    ans += 11 - num;
-                    return;
+                    if(sum < k)
+                    {
+                        right++;
+                        if (right <= sequence.Length - 1)
+                            sum += sequence[right];
+                    }
+                    else if (sum > k)
+                    {
+                        sum -= sequence[left];
+                        left++;
+                    }
+                    else
+                    {
+                        // 일치하는 조합을 기록한 뒤, left ++ 해서 다음 조합을 찾기 << 이때 right가 left보다 작아져도 break;
+                        // 똑같은 조합을 한번더 마주치면 break로 종료
+                        if (!pairs.Contains(Tuple.Create(left, right)))
+                        {
+                            if(right - left < minDiff) minDiff = right - left; // 최소 길이 갱신
+                            pairs.Add(Tuple.Create(left, right));
+                            if(right == left)
+                            {
+                                right++;
+                                if (right <= sequence.Length - 1)
+                                {
+                                    sum += sequence[right];
+                                }
+                                else
+                                    break;
+                            }
+                            else if(left < right)
+                            {
+                                sum -= sequence[left];
+                                if (left < right)
+                                    left++;
+                                else
+                                    break;
+                            }
+                            else
+                                break;
+                        }
+                        else
+                            break;
+                    }
                 }
+
+                // 기록된 left, right 조합들 선별작업 진행
+                // 1. 후보군들 중 길이가 짧은 수열
+                List<Tuple<int, int>> toRemove = new List<Tuple<int, int>>();
+                foreach (var pair in pairs)
+                {
+                    int a = pair.Item1;
+                    int b = pair.Item2;
+                    if (b - a > minDiff)
+                        toRemove.Add(pair);
+                }
+
+                foreach (var pair in toRemove)
+                    pairs.Remove(pair);
+
+                // 2. 시작 인덱스의 값이 가장 낮은 수열
+                foreach (var pair in pairs)
+                {
+                    tupleStart = pairs.Min(p => p.Item1);
+                }
+                left = tupleStart;
+                right = left + minDiff;
+
+                return new int[] { left, right };
+            }
+        }
+
+        public class Solution4
+        {
+            public int[] solution(int[] sequence, int k)
+            {
+                int[] answer = new int[] { };
+                int minDiff = int.MaxValue;
+                int left = 0;
+                int right = 0;
+                long sum = sequence[left];
+                int ansLeft = 0; int ansRight = 0;
+
+
+                while (left < sequence.Length && right < sequence.Length)
+                {
+                    if (sum < k)
+                    {
+                        right++;
+                        if (right <= sequence.Length - 1)
+                            sum += sequence[right];
+                        else
+                            break;
+                    }
+                    else if (sum > k)
+                    {
+                        sum -= sequence[left];
+                        left++;
+                        if (left > right && left < sequence.Length)
+                        {
+                            right = left;
+                            sum = sequence[right];
+                        }
+                    }
+                    else // sum == k
+                    {
+                        // 일치하는 조합을 기록한 뒤, left ++ 해서 다음 조합을 찾기 << 이때 right가 left보다 작아져도 break;
+                        // 똑같은 조합을 한번더 마주치면 break로 종료
+                        int currentDiff = right - left;
+                        if (currentDiff < minDiff)
+                        {
+                            minDiff = currentDiff;
+                            ansLeft = left;
+                            ansRight = right;
+                        }
+
+                        sum -= sequence[left];
+                        left++;
+
+                        if (left > right && left < sequence.Length)
+                        {
+                            right = left;
+                            sum = sequence[right];
+                        }
+                    }
+                }
+
+                return new int[] { ansLeft, ansRight };
             }
         }
 
         static void Main(string[] args)
         {
-            Solution solution = new Solution();
-            int result = solution.solution(45);
-            System.Console.WriteLine(result);
+            Solution3 solution = new Solution3();
+            int[] ans = solution.solution(new int[] { 1,2,3,4,5},7);
+            Console.WriteLine(string.Join(", ", ans));
         }
     }
 }
