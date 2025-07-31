68df016cc4fa6b9a17618d206b089d1dbd336d5c
kim minjun <k980128@naver.com>
2025-07-31 16:30:44 +0900

    Coding test
    https://school.programmers.co.kr/learn/courses/30/lessons/148653

--- Code/Code/code/Program.cs
+++ Code/Code/code/Program.cs
@@ -8,60 +8,86 @@ namespace code
     internal class Program
     {
         /// <summary>
-        /// https://school.programmers.co.kr/learn/courses/30/lessons/152996
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/148653
         /// </summary>
 
         public class Solution
         {
-            public long solution(int[] weights)
+            public int solution(int storey)
             {
-                long answer = 0;
-                var count = new Dictionary<int, long>();
-
-                foreach (int w in weights)
+                int answer = 0;
+                while (storey / 10 > 0)
                 {
-                    if (!count.ContainsKey(w)) count[w] = 0;
-
-                    count[w]++;
+                    Stone(ref storey ,storey % 10, ref answer);
+                    storey /= 10;
                 }
-
-                var ratios = new Tuple<int, int>[]
-                {
-                    Tuple.Create(1, 1),
-                    Tuple.Create(2, 3),
-                    Tuple.Create(3, 4),
-                    Tuple.Create(1, 2),
-                    Tuple.Create(3, 2),
-                    Tuple.Create(4, 3),
-                };
-
-
-                foreach (var ratio in ratios)
-                {
-                    int a = ratio.Item1;
-                    int b = ratio.Item2;
-
-                    foreach (int w in count.Keys)
-                    {
-                        long wCount = count[w];
-                        if ((w * b) % a != 0) continue; // 불가능한 케이스 건너뛰기
-
-                        int target = (w * b) / a;
-                        if (!count.ContainsKey(target)) continue; // 등록된 값에 없으면 건너뛰기
-
-                        if (w == target)
-                        {
-                            answer += wCount * (wCount - 1) / 2;
-                        }
-                        else if (w < target)
-                        {
-                            answer += wCount * count[target];
-                        }
-                    }
-                }
-
+                LastStone(storey, ref answer);
                 return answer;
             }
+
+            void Stone(ref int ori,int num, ref int ans)
+            {
+                if(num == 0)
+                {
+                    return;
+                }
+                if(num <= 4)
+                {
+                    ans += num;
+                    return;
+                }
+                if(num == 5)
+                {
+                    if (ori / 10 != 0)
+                    {
+                        if((ori /10) % 10 >= 5)
+                        {
+                            ans += 5;
+                            ori += 10;
+                        }
+                        else
+                        {
+                            ans += 5;
+                        }
+                    }
+                    else
+                    {
+                        ans += 5;
+                    }
+                    return;
+                }
+                if(num >= 6)
+                {
+                    ans += 10 - num;
+                    ori += 10;
+                    return;
+                }
+            }
+
+            void LastStone(int num, ref int ans)
+            {
+                if (num == 0)
+                {
+                    return;
+                }
+                if (num <= 5)
+                {
+                    ans += num;
+                    return;
+                }  
+                if (num >= 6)
+                {
+                    ans += 11 - num;
+                    return;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int result = solution.solution(45);
+            System.Console.WriteLine(result);
         }
     }
 }
