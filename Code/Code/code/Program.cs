9130fc6d42f24471bcb698d9f0afe43598cd5780
kim minjun <k980128@naver.com>
2025-08-10 20:41:14 +0900

    Coding test
    https://school.programmers.co.kr/learn/courses/30/lessons/155651?language=csharp

--- Code/Code/code/Program.cs
+++ Code/Code/code/Program.cs
@@ -12,44 +12,67 @@ namespace code
     internal class Program
     {
         /// <summary>
-        /// https://school.programmers.co.kr/learn/courses/30/lessons/389479
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/155651?language=csharp
         /// </summary>
 
         public class Solution
         {
-            public int solution(int[] players, int m, int k)
+            public int solution(string[,] book_time)
             {
-                
-                Queue<int> serverQ = new Queue<int>();
-                int currentServer = 0;
-                int cumuSever = 0;
-                for(int i = 0; i < players.Length; i++)
-                {
-                    // 유지 시간 이후부터는 계속 시간 지난 서버 제거
-                    if (i - k >= 0) currentServer -= serverQ.Dequeue();
+                // book_time을 입장 시간 순으로 정렬
+                int length = book_time.GetLength(0);
+                var timeList = new List<Tuple<string, string>>();
 
-                    int total = 0;
-                    // 총 유저수가 서버의 가용량을 넘어설때
-                    if (players[i] >= (currentServer+1) * m)
+                for (int i = 0; i < length; i++)
+                {
+                    timeList.Add(Tuple.Create(book_time[i, 0], book_time[i, 1]));
+                }
+
+                var sorted = timeList.OrderBy(t => TimeToMinutes(t.Item1)).ToArray();
+
+                // 방 배정 시작
+                Dictionary<int,int> rooms = new Dictionary<int, int>(); // key : 방번호, value : 끝나는 시간 << 비교시에는 시작하는 시간으로 비교하여 내용물 교체.
+                rooms.Add(0, 0); // 방번호 0번은 00:00에 미리 등록
+
+                for (int i = 0; i < sorted.GetLength(0); i++)
+                {
+                    int startTime = TimeToMinutes(sorted[i].Item1); // 시작 시간
+                    int endTime = TimeToMinutes(sorted[i].Item2); // 끝나는 시간
+
+                    // 방번호를 찾기
+                    int roomNumber = -1;
+                    for(int j = 0; j < rooms.Count; j++)
                     {
-                        while (players[i] >= (currentServer + 1) * m)
+                        if ((rooms[j] + 10) - startTime <= 0)
                         {
-                            currentServer++;
-                            cumuSever++;
-                            total++;
+                            roomNumber = j; // 해당 방 번호를 저장
+                            break;
                         }
                     }
-                    serverQ.Enqueue(total);
+
+                    // 사용 가능한 방이 없다면 새 방을 추가
+                    if (roomNumber == -1) roomNumber = rooms.Count;
+
+                    rooms[roomNumber] = endTime;
                 }
-                return cumuSever;
+
+                return rooms.Count;
+            }
+
+            int TimeToMinutes(string time)
+            {
+                string[] parts = time.Split(':');
+                int hours = int.Parse(parts[0]);
+                int minutes = int.Parse(parts[1]);
+                return hours * 60 + minutes;
             }
 
 
             static void Main(string[] args)
             {
                 Solution solution = new Solution();
-                int ans = solution.solution([0, 2, 3, 3, 1, 2, 0, 0, 0, 0, 4, 2, 0, 6, 0, 4, 2, 13, 3, 5, 10, 0, 1, 5], 3,5);
-                Console.WriteLine(ans);
+                //int ans = solution.solution([["15:00", "17:00"], ["16:40", "18:20"], ["14:20", "15:20"], ["14:10", "19:20"], ["18:20", "21:20"]]);
+                //Console.WriteLine(ans);
             }
         }
     }
