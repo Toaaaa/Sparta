0839e39ef5494fe0d0055c230a4f9091233a28fe
kim minjun <k980128@naver.com>
2025-08-22 22:33:14 +0900

    Coding test
    https://school.programmers.co.kr/learn/courses/30/lessons/49994

--- Code/Code/code/Program.cs
+++ Code/Code/code/Program.cs
@@ -12,37 +12,80 @@ namespace code
     internal class Program
     {
         /// <summary>
-        /// https://school.programmers.co.kr/learn/courses/30/lessons/12980
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/49994
         /// </summary>
 
         public class Solution
         {
-            public int solution(int n)
+            public int solution(string dirs)
             {
-                int answer = 0;
-                answer++;
+                Tuple<int, int> current = new Tuple<int, int>(0, 0);
+                HashSet<Tuple<int, int, string>> visited = new HashSet<Tuple<int, int, string>>();
 
-                while (n > 1)
+                for(int i = 0; i < dirs.Length; i++)
                 {
-                    if(n % 2 == 0)
+                    char direction = dirs[i];
+                    switch (direction)
                     {
-                        n /= 2;
-                    }
-                    else
-                    {
-                        n -= 1;
-                        answer++;
+                        case 'U':
+                            if(current.Item1 < 5)
+                            {
+                                var next = new Tuple<int, int>(current.Item1 + 1, current.Item2);
+                                if (!visited.Contains(new Tuple<int, int, string>(current.Item1, current.Item2, "U")) && 
+                                    !visited.Contains(new Tuple<int, int, string>(next.Item1, next.Item2, "D")))
+                                {
+                                    visited.Add(new Tuple<int, int, string>(current.Item1, current.Item2, "U"));
+                                }
+                                current = next;
+                            }
+                            break;
+                        case 'D':
+                            if(current.Item1 > -5)
+                            {
+                                var next = new Tuple<int, int>(current.Item1 - 1, current.Item2);
+                                if (!visited.Contains(new Tuple<int, int, string>(current.Item1, current.Item2, "D")) && 
+                                    !visited.Contains(new Tuple<int, int, string>(next.Item1, next.Item2, "U")))
+                                {
+                                    visited.Add(new Tuple<int, int, string>(current.Item1, current.Item2, "D"));
+                                }
+                                current = next;
+                            }
+                            break;
+                        case 'L':
+                            if(current.Item2 > -5)
+                            {
+                                var next = new Tuple<int, int>(current.Item1, current.Item2 - 1);
+                                if (!visited.Contains(new Tuple<int, int, string>(current.Item1, current.Item2, "L")) && 
+                                    !visited.Contains(new Tuple<int, int, string>(next.Item1, next.Item2, "R")))
+                                {
+                                    visited.Add(new Tuple<int, int, string>(current.Item1, current.Item2, "L"));
+                                }
+                                current = next;
+                            }
+                            break;
+                        case 'R':
+                            if(current.Item2 < 5)
+                            {
+                                var next = new Tuple<int, int>(current.Item1, current.Item2 + 1);
+                                if (!visited.Contains(new Tuple<int, int, string>(current.Item1, current.Item2, "R")) && 
+                                    !visited.Contains(new Tuple<int, int, string>(next.Item1, next.Item2, "L")))
+                                {
+                                    visited.Add(new Tuple<int, int, string>(current.Item1, current.Item2, "R"));
+                                }
+                                current = next;
+                            }
+                            break;
                     }
                 }
-
-                return answer;
+                return visited.Count;
             }
 
             static void Main(string[] args)
             {
                 Solution solution = new Solution();
+                string dirs = "ULURRDLLU";
 
-                int answer = solution.solution(5000);
+                int answer = solution.solution(dirs);
                 Console.WriteLine(answer);
             }
         }
