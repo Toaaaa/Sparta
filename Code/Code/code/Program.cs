967ee1a557531054325a1f76786fcc55e1022155
kim minjun <k980128@naver.com>
2025-09-09 23:58:28 +0900

    Coding test
    https://school.programmers.co.kr/learn/courses/30/lessons/132265

--- Code/Code/code/Program.cs
+++ Code/Code/code/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Formats.Asn1;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
@@ -12,55 +13,55 @@ namespace code
     internal class Program
     {
         /// <summary>
-        /// https://school.programmers.co.kr/learn/courses/30/lessons/1844
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/132265
         /// </summary>
 
         public class Solution
         {
-            public int solution(int[,] maps)
+            public int solution(int[] topping)
             {
-                int n = maps.GetLength(0);
-                int m = maps.GetLength(1);
+                int n = topping.Length;
+                int answer = 0;
 
-                int[,] visited = new int[n, m]; // 이동 횟수를 기록하는 배열
-                Queue<(int x, int y)> q = new Queue<(int, int)>();
-                q.Enqueue((0, 0));
-                visited[0, 0] = 1;
-
-                int[] dx = new int[] { 0, 0, 1, -1 }; // 상하우좌
-                int[] dy = new int[] { 1, -1, 0, 0 }; // 상하우좌
-
-                while (q.Count > 0)
+                // 오른쪽 토핑 개수 카운트
+                Dictionary<int, int> rightCount = new Dictionary<int, int>();
+                foreach (int t in topping)
                 {
-                    var (x, y) = q.Dequeue();
-
-                    for (int dir = 0; dir < 4; dir++)
-                    {
-                        int nx = x + dx[dir];
-                        int ny = y + dy[dir];
-
-                        if (nx >= 0 && ny >= 0 && nx < n && ny < m)
-                        {
-                            if (maps[nx, ny] == 1 && visited[nx, ny] == 0)
-                            {
-                                visited[nx, ny] = visited[x, y] + 1;
-                                q.Enqueue((nx, ny));
-                            }
-                        }
-                    }
+                    if (rightCount.ContainsKey(t)) rightCount[t]++;
+                    else rightCount[t] = 1;
                 }
 
-                return visited[n - 1, m - 1] == 0 ? -1 : visited[n - 1, m - 1];
-                // queue를 다 돌렸는데도 도착지점에 도달 못했으면 -1 리턴 그게 아니면 이동 횟수 리턴.
+                HashSet<int> leftSet = new HashSet<int>();
+                int leftDistinct = 0;
+                int rightDistinct = rightCount.Count;
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    int t = topping[i];
+
+                    // 왼쪽에 추가
+                    if (leftSet.Add(t))
+                        leftDistinct++;
+
+                    // 오른쪽에서 제거
+                    rightCount[t]--;
+                    if (rightCount[t] == 0)
+                        rightDistinct--;
+
+                    // 비교
+                    if (leftDistinct == rightDistinct)
+                        answer++;
+                }
+
+                return answer;
             }
 
             static void Main(string[] args)
             {
                 Solution solution = new Solution();
-                //string dirs = "ULURRDLLU";
 
-                //int answer = solution.solution(dirs);
-                //Console.WriteLine(answer);
+                int answer = solution.solution([1, 2, 3, 1, 4]);
+                Console.WriteLine(answer);
             }
         }
     }
