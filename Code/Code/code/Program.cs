dbdb828e17c21662fed10faa2d26b8c84932ee18
kim minjun <k980128@naver.com>
2025-09-12 19:47:21 +0900

    Coding test
    https://school.programmers.co.kr/learn/courses/30/lessons/42579

--- Code/Code/code/Program.cs
+++ Code/Code/code/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Formats.Asn1;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -13,36 +14,49 @@ namespace code
     internal class Program
     {
         /// <summary>
-        /// https://school.programmers.co.kr/learn/courses/30/lessons/49993
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/42579
         /// </summary>
 
         public class Solution
         {
-            public int solution(string skill, string[] skill_trees)
+            public int[] solution(string[] genres, int[] plays)
             {
-                int answer = 0;
-                Queue<char> skillQueue = new Queue<char>(skill.ToCharArray());
-                Queue<char> tempQueue = new Queue<char>();
+                int[] answer = new int[] { };
+                List<Tuple<string,int,int>> list = new List<Tuple<string,int,int>>(); // Tuple<장르 ,재생횟수, 고유번호>
+                Dictionary<string,int> genreDict = new Dictionary<string,int>(); // 장르별 총합 재생횟수
+                Dictionary<string, int> checkdDict = new Dictionary<string, int>(); // 장르별 누적 채택 횟수
 
-                for (int i = 0; i < skill_trees.Length; i++)
+                for(int i = 0; i < genres.Length; i++)
                 {
-                    answer++;
-                    tempQueue = new Queue<char>(skillQueue);
-                    for(int j =0; j < skill_trees[i].Length; j++)
+                    list.Add(new Tuple<string,int, int>(genres[i],plays[i], i));
+
+                    if (genreDict.ContainsKey(genres[i]))
+                        genreDict[genres[i]] += plays[i];
+                    else
                     {
-                        // 다음 진행 스킬이, 스킬 트리에 포함된 스킬인 경우
-                        if (skill.Contains(skill_trees[i][j]))
+                        genreDict.Add(genres[i], plays[i]);
+                        checkdDict.Add(genres[i], 2);
+                    }
+                }
+                list.Sort((a, b) => b.Item2.CompareTo(a.Item2)); // 재생횟수 내림차순 정렬
+                var sortedGenre = genreDict.OrderByDescending(x => x.Value).Select(x => x.Key).ToList(); // 장르별 재생횟수 내림차순으로 list에 받아오기
+
+
+                List<int> ans = new List<int>();
+                for (int i = 0; i < sortedGenre.Count; i++)
+                {
+                    for (int j = 0; j < genres.Length; j++)
+                    {
+                        if (list[j].Item1 == sortedGenre[i] && checkdDict[sortedGenre[i]] > 0)
                         {
-                            if (tempQueue.Count > 0 && skill_trees[i][j] == tempQueue.Peek())
-                                tempQueue.Dequeue();
-                            else
-                            {
-                                answer--;
-                                break;
-                            }
+                            ans.Add(list[j].Item3);
+                            checkdDict[sortedGenre[i]]--;
+                            if(checkdDict[sortedGenre[i]] == 0) break;
                         }
                     }
                 }
+
+                answer = ans.ToArray();
                 return answer;
             }
 
