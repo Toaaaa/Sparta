223b3289a73919cc430099a3c36ac3f2dc8937cb
kim minjun <k980128@naver.com>
2025-09-17 20:57:33 +0900

    Codint test
    https://school.programmers.co.kr/learn/courses/30/lessons/12971

--- Code/Code/code/Program.cs
+++ Code/Code/code/Program.cs
@@ -14,86 +14,37 @@ namespace code
     internal class Program
     {
         /// <summary>
-        /// https://school.programmers.co.kr/learn/courses/30/lessons/12987
+        /// https://school.programmers.co.kr/learn/courses/30/lessons/12971
         /// </summary>
 
         public class Solution
         {
-            // 1. Dictionary 만 사용한 풀이
-            public int solution(int[] A, int[] B)
+            public int solution(int[] sticker)
             {
-                int answer = 0;
-                Dictionary<int, int> dictA = new Dictionary<int, int>();
-                for(int i = 0; i < B.Length; i++)
-                {
-                    if (dictA.ContainsKey(B[i]))
-                        dictA[B[i]]++;
-                    else
-                        dictA.Add(B[i], 1);
-                }
-                for (int i = 0; i < A.Length; i++)
-                {
-                    if (dictA.Count == 0) break;
+                int n = sticker.Length;
+                if (n == 1) return sticker[0];
 
-                    var key = dictA.Keys.FirstOrDefault(k => k > A[i]);
-
-                    if (key == 0) dictA.Remove(dictA.Keys.First()); // A[i]보다 큰 값이 없으면 가장 작은 값 대입
-                    else
-                    {
-                        answer++;
-                        if (dictA[key] == 1)
-                            dictA.Remove(key);
-                        else
-                            dictA[key]--;
-                    }
+                // 1. 첫 번째 스티커 선택
+                int[] dp1 = new int[n];
+                dp1[0] = sticker[0];
+                dp1[1] = Math.Max(sticker[0], sticker[1]);
+                for (int i = 2; i < n - 1; i++)
+                {
+                    dp1[i] = Math.Max(dp1[i - 1], dp1[i - 2] + sticker[i]);
                 }
-                return answer;
+
+                // 2. 첫 번째 스티커 선택하지 않음
+                int[] dp2 = new int[n];
+                dp2[0] = 0;
+                dp2[1] = sticker[1];
+                for (int i = 2; i < n; i++)
+                {
+                    dp2[i] = Math.Max(dp2[i - 1], dp2[i - 2] + sticker[i]);
+                }
+
+                return Math.Max(dp1[n - 2], dp2[n - 1]);
             }
 
-
-            // 2. SortedDictionary + SortedSet 사용한 풀이 (시간 복잡도 고려)
-            public int solution2(int[] A, int[] B)
-            {
-                int answer = 0;
-                SortedDictionary<int, int> dictA = new SortedDictionary<int, int>();
-                SortedSet<int> keys = new SortedSet<int>();
-
-                for (int i = 0; i < B.Length; i++)
-                {
-                    if (dictA.ContainsKey(B[i]))
-                        dictA[B[i]]++;
-                    else
-                        dictA.Add(B[i], 1);
-
-                    keys.Add(B[i]);
-                }
-
-                for (int i = 0; i < A.Length; i++)
-                {
-                    if (dictA.Count == 0) break;
-
-                    int key;
-
-                    // A[i]보다 큰 값 찾기
-                    var view = keys.GetViewBetween(A[i] + 1, int.MaxValue);
-                    if (view.Count > 0)
-                    {
-                        key = view.Min;
-                        answer++;
-                    }
-                    else
-                        key = keys.Min; // A[i]보다 큰 값이 없으면 가장 작은 값 대입
-
-                    if (dictA[key] == 1)
-                    {
-                        dictA.Remove(key);
-                        keys.Remove(key);
-                    }
-                    else
-                        dictA[key]--;
-                }
-                return answer;
-            }
 
 
 
